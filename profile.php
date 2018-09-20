027c437b546d24d853b53af3a5a3f833ead870eb
partitan <partitan@gmail.com>
2018-09-20 21:25:50 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,8 @@
 <?php
 
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$access_token = 'CEdTKiv9KD/q/C4xWu9hEUbI/RxXH026qprWmcyqgGwuFz6fXfXPuo+ND2WPBNiGDgjSRjtXHiZDaKp62GfpwKjm5/GBdcI4LuvzkB1rFfTzSBtkLaN6/xxE9EU8A0/Mf4+rQb4ZiMOH9eij2kFeFQdB04t89/1O/w1cDnyilFU=';
+$userId = 'U5d4cc13c144894cd4197bffbcc197d9e';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
